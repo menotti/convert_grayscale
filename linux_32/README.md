fef60b11a82ce1000e07633ff199d12f6042cdbb
Virtual Lab <menotti@gmail.com>
2022-02-18 11:12:19 -0300

    fix & traducao dos comentarios

--- linux_32/README.md
+++ linux_32/README.md
@@ -2,3 +2,4 @@
 
 ## References
 * https://www.tutorialspoint.com/assembly_programming/assembly_file_management.htm
+* https://chromium.googlesource.com/chromiumos/docs/+/master/constants/syscalls.md#x86-32_bit
--- linux_32/write.asm
+++ linux_32/write.asm
@@ -1,74 +1,74 @@
-section	.text
-   global _start         ;must be declared for using gcc
-	
-_start:                  ;tell linker entry point
-   ;create the file
-   mov  eax, 8
-   mov  ebx, file_name
-   mov  ecx, 0777        ;read, write and execute by all
-   int  0x80             ;call kernel
-	
-   mov [fd_out], eax
-    
-   ; write into the file
-   mov	edx,len          ;number of bytes
-   mov	ecx, msg         ;message to write
-   mov	ebx, [fd_out]    ;file descriptor 
-   mov	eax,4            ;system call number (sys_write)
-   int	0x80             ;call kernel
-	
-   ; close the file
-   mov eax, 6
-   mov ebx, [fd_out]
-    
-   ; write the message indicating end of file write
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, msg_done
-   mov edx, len_done
-   int  0x80
-    
-   ;open the file for reading
-   mov eax, 5
-   mov ebx, file_name
-   mov ecx, 0             ;for read only access
-   mov edx, 0777          ;read, write and execute by all
-   int  0x80
-	
-   mov  [fd_in], eax
-    
-   ;read from file
-   mov eax, 3
-   mov ebx, [fd_in]
-   mov ecx, info
-   mov edx, 26
-   int 0x80
-    
-   ; close the file
-   mov eax, 6
-   mov ebx, [fd_in]
-   int  0x80    
-	
-   ; print the info 
-   mov eax, 4
-   mov ebx, 1
-   mov ecx, info
-   mov edx, 26
-   int 0x80
-       
-   mov	eax,1             ;system call number (sys_exit)
-   int	0x80              ;call kernel
+section .text
+   global main           ; precisa ser declarado para o gcc
 
-section	.data
-file_name db 'myfile.txt'
-msg db 'Welcome to Tutorials Point'
-len equ  $-msg
+main:                    ; informa o linker sobre o procedimento de entrada
+   ; cria o arquivo
+   mov eax, 8            ; chamada de sistema (sys_creat)
+   mov ebx, file_name    ; nome do arquivo
+   mov ecx, 664o         ; leitura e escrita para usuario e grupo, somente leitura para os demais (sufixo 'o' para base octal)
+   int 0x80              ; interrupcao
+   mov [fd_out], eax     ; salva o descritor do arquivo que retornou
+    
+   ; escreve no arquivo
+   mov eax, 4            ; chamada de sistema (sys_write)
+   mov ebx, [fd_out]     ; descritor do arquivo
+   mov ecx, msg          ; conteudo a ser escrito
+   mov edx, len          ; numero de bytes
+   int 0x80
+
+   ; fecha o arquivo
+   mov eax, 6            ; chamada de sistema (sys_close)
+   mov ebx, [fd_out]     ; descritor do arquivo
+   int 0x80
+
+   ; imprime mensagem indicando escrita do arquivo
+   mov eax, 4            ; chamada de sistema (sys_write)
+   mov ebx, 1            ; descritor (saida padrao)
+   mov ecx, msg_done     ; conteudo a ser escrito
+   mov edx, len_done     ; numero de bytes
+   int  0x80
+    
+   ; abre arquivo para leitura
+   mov eax, 5            ; chamada de sistema (sys_open)
+   mov ebx, file_name    ; nome do arquivo
+   mov ecx, 0            ; apenas para leitura
+   mov edx, 664o         ; leitura e escrita para usuario e grupo, somente leitura para os demais (sufixo 'o' para base octal)
+   int  0x80
+   mov  [fd_in], eax     ; salva o descrito do arquivo que retornou
+    
+   ; le do arquivo
+   mov eax, 3            ; chamada de sistema (sys_read)
+   mov ebx, [fd_in]      ; descritor do arquivo
+   mov ecx, info         ; buffer que recebe os dados lidos
+   mov edx, 27           ; numero de bytes
+   int 0x80
+    
+   ; fecha o arquivo
+   mov eax, 6            ; chamada de sistema (sys_close)
+   mov ebx, [fd_in]      ; descritor do arquivo
+   int 0x80
+
+   ; imprime a informacao lida
+   mov eax, 4            ; chamada de sistema (sys_write)
+   mov ebx, 1            ; descritor (saida padrao)
+   mov ecx, info         ; conteudo a ser escrito
+   mov edx, 27           ; numero de bytes
+   int 0x80
+
+   ; encerra o programa
+   mov eax, 0            ; chamada de sistema (sys_exit)
+   int 0x80
+
+section .data
+file_name db 'myfile.txt', 0x0
+msg db 'Welcome to Tutorials Point', 0xa
+len equ $-msg
 
 msg_done db 'Written to file', 0xa
 len_done equ $-msg_done
 
 section .bss
-fd_out resb 1
-fd_in  resb 1
-info resb  26
+fd_out resb  1
+fd_in  resb  1
+info   resb  len
 
