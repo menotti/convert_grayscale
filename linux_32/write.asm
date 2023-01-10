e48c4c57f969184be438aaaf825557d8a1a76b61
Ricardo Menotti <menotti@gmail.com>
2023-01-10 15:42:42 -0300

    Fix minor error

--- linux_32/write.asm
+++ linux_32/write.asm
@@ -1,7 +1,7 @@
 section .text
-   global main           ; precisa ser declarado para o gcc
+   global _start         ; precisa ser declarado para o gcc
 
-main:                    ; informa o linker sobre o procedimento de entrada
+_start:                  ; informa o linker sobre o procedimento de entrada
    ; cria o arquivo
    mov eax, 8            ; chamada de sistema (sys_creat)
    mov ebx, file_name    ; nome do arquivo
@@ -40,7 +40,7 @@ main:                    ; informa o linker sobre o procedimento de entrada
    mov eax, 3            ; chamada de sistema (sys_read)
    mov ebx, [fd_in]      ; descritor do arquivo
    mov ecx, info         ; buffer que recebe os dados lidos
-   mov edx, 27           ; numero de bytes
+   mov edx, len          ; numero de bytes
    int 0x80
     
    ; fecha o arquivo
@@ -52,20 +52,31 @@ main:                    ; informa o linker sobre o procedimento de entrada
    mov eax, 4            ; chamada de sistema (sys_write)
    mov ebx, 1            ; descritor (saida padrao)
    mov ecx, info         ; conteudo a ser escrito
-   mov edx, 27           ; numero de bytes
+   mov edx, len          ; numero de bytes
    int 0x80
 
+   ; imprime mensagem de debug
+   mov eax, 4            ; chamada de sistema (sys_write)
+   mov ebx, 1            ; descritor (saida padrao)
+   mov ecx, msg_debug    ; conteudo a ser escrito
+   mov edx, len_debug    ; numero de bytes
+   int  0x80
+    
    ; encerra o programa
-   mov eax, 0            ; chamada de sistema (sys_exit)
+   mov eax, 1            ; chamada de sistema (sys_exit)
+   mov ebx, 0
    int 0x80
 
 section .data
 file_name db 'myfile.txt', 0x0
-msg db 'Welcome to Tutorials Point', 0xa
+msg db 'Welcome to Tutorials Point (corrected)!', 0xa
 len equ $-msg
 
 msg_done db 'Written to file', 0xa
 len_done equ $-msg_done
+
+msg_debug db 'Debug...', 0xa
+len_debug equ $-msg_debug
 
 section .bss
 fd_out resb  1
